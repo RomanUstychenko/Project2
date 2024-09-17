f179c2da3e0293694ffb84c9271a1ee2e680eb17
Roman <roman.ustychenko@gmail.com>
2024-09-17 12:22:07 +0300

    create-react-app typescript

---
+++ my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to my React TypeScript App!</h1>
+        <p>Let's start building amazing things 🚀</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
